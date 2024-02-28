00901bc19d15622acf723f7b38c31aadd7d6f664
max <63799585+minman302@users.noreply.github.com>
2024-02-28 16:30:39 -0500

    motor control update
    Bluemotor proportional control added, new function with deadband removal

--- BlueMotorClass One Encoder Template/src/BlueMotor.h
+++ BlueMotorClass One Encoder Template/src/BlueMotor.h
@@ -5,19 +5,27 @@ class BlueMotor
 public:
     BlueMotor();
     void setEffort(int effort);
-    void moveTo(long position);
+    void setEffortTuned(int effort);
+    int moveTo(long position);
     long getPosition();
     void reset();
     void setup();
 
 private:
     void setEffort(int effort, bool clockwise);
-    static void isr();
+    static void isr1();
+    static void isr2();
+    static void isr3();
+    static void isr4();
+    
+
+
     const int tolerance = 3;
     const int PWMOutPin = 11;
     const int AIN2 = 4;
     const int AIN1 = 13;
     const int ENCA = 0;
     const int ENCB = 1;
-    float Kp = 2;
+    const int deadBandAjust = 100;
+    float Kp = 1.5;
 };
