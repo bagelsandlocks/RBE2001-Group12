1050a10e221227fa40b54646f8915906d2402e26
bagelsandlocks <121209518+bagelsandlocks@users.noreply.github.com>
2024-02-28 15:41:12 -0500

    created lineFollow.h

---
+++ BlueMotorClass One Encoder Template/src/LineFollow.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class LineFollow
+{
+public:
+    LineFollow();
+    void lineFollow();
+    void setup();
+    void findLine();
+    int leftReflectanceAverage();
+    int rightReflectanceAverage();
+
+private:
+    
+};
